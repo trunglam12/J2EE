aa0beb436c3d8bff36509524eee07ac9566f72fc
Belshazzar <belshazzar684@gmail.com>
2016-06-12 23:25:08 +0700

    add login/logout delete jar file in build

--- Do An J2EE/Coffee/Coffee-ejb/src/java/SessionBean/UserFacade.java
+++ Do An J2EE/Coffee/Coffee-ejb/src/java/SessionBean/UserFacade.java
@@ -6,11 +6,13 @@
 package SessionBean;
 
 import entities.User;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -35,4 +37,39 @@ public class UserFacade extends AbstractFacade<User> {
         super(User.class);
     }
     
+        public boolean IsUserAccountExist(String userAccount){
+        Query query = getEntityManager().createNamedQuery("User.findByUserAccount");
+        query.setParameter("userAccount", userAccount);
+        List<User> result = query.getResultList();
+        if (result.size() != 0) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    public User FindUser(String userAccount, String password) {
+        Query query = getEntityManager().createNamedQuery("User.findByUserAccount");
+        query.setParameter("userAccount", userAccount);
+        List<User> result = query.getResultList();
+        if (result.size() != 0) {
+            return result.get(0).getPassword().equals(password) ? result.get(0) : null;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean Register(String userAccount, String password) {
+        try {
+            User user = new User();
+            user.setUserAccount(userAccount);
+            user.setPassword(password);
+            super.create(user);
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
 }
---
+++ Do An J2EE/Coffee/Coffee-war/src/java/Controller/LoginManagedBean.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import SessionBean.UserFacade;
+import entities.User;
+import java.io.Serializable;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ *
+ * @author belsh
+ */
+@ManagedBean(name = "loginBean")
+@SessionScoped
+public class LoginManagedBean {
+        @EJB
+    private UserFacade userFacade;
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 2951813936936766650L;
+
+    public LoginManagedBean() {
+        System.out.println("LoginBean()");
+    }
+
+    private String username;
+    private String password;
+    private String confirmPassword;
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getUsername() {
+        System.out.println("getUsername() returning: " + this.username);
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        System.out.println("setUserName(" + username + ")");
+        this.username = username;
+    }
+
+    public String getPassword() {
+        System.out.println("getPassword() returning: " + this.password);
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        System.out.println("setPassword(" + password + ")");
+        this.password = password;
+    }
+
+    public String Login() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        User user = userFacade.FindUser(username, md5(password));
+        if (user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", username);
+            return "/Product/ProductManagement?face-redirect=true";
+        }
+        else{
+            FacesMessage facemsg= new FacesMessage(FacesMessage.SEVERITY_ERROR, "Tên tài khoản hoặc mật khẩu không chính xác", "Tên tài khoản hoặc mật khẩu không chính xác");
+            context.addMessage(null, facemsg);
+        }
+        return "/login?face-redirect=true";
+    }
+    
+    public String Register() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        if(userFacade.IsUserAccountExist(username)){
+            if(!password.equals(confirmPassword)){
+                FacesMessage facemsg= new FacesMessage(FacesMessage.SEVERITY_ERROR, "Mật khẩu và mật khẩu xác nhận không khớp", "Mật khẩu và mật khẩu xác nhận không khớp");
+                context.addMessage(null, facemsg);
+                return "/login?face-redirect=true#signup";
+            }
+            if(userFacade.Register(username, md5(password))){
+                HttpSession session = request.getSession();
+                session.setAttribute("user", username);
+                return "/Product/ProductManagement?face-redirect=true";
+            }
+            else{
+                FacesMessage facemsg= new FacesMessage(FacesMessage.SEVERITY_ERROR, "Có lỗi xảy ra khi thực hiện yêu cầu của bạn", "Có lỗi xảy ra khi thực hiện yêu cầu của bạn");
+                context.addMessage(null, facemsg);
+            }
+        }
+        else{
+                FacesMessage facemsg= new FacesMessage(FacesMessage.SEVERITY_ERROR, "Tên tài khoản đã tồn tai", "Tên tài khoản đã tồn tai");
+                context.addMessage(null, facemsg);
+        }
+        return "/login?face-redirect=true#signup";
+    }
+
+    public String Logout() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        HttpSession session = request.getSession();
+        session.setAttribute("user", null);
+        return "/login?face-redirect=true";
+    }
+    
+    public String md5(String data)
+    {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(data.getBytes());
+            
+            byte byteData[] = md.digest();
+            
+            //convert the byte to hex format method 1
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(LoginManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+}
